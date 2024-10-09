199fc864324e686ce3c20d3a488fee49f637d315
kevintsoii <kevin.tsoi44@gmail.com>
2024-10-08 19:09:43 -0700

    fix users router naming

--- server/src/router/v1/users.ts
+++ server/src/router/v1/users.ts
@@ -3,15 +3,15 @@ import { db } from '../../db/db';
 import { users } from '../../db/schema';
 import type { User } from '../../db/schema';
 
-const users = new Hono();
+const usersRouter = new Hono();
 
-users.get('/', async c => {
+usersRouter.get('/', async c => {
     const foundUsers: User[] = await db.select().from(users);
 	return c.json({
 		users: foundUsers,
 	});
 });
 
-users.get('/rental-history', c => c.json({}));
+usersRouter.get('/rental-history', c => c.json({}));
 
-export default users;
+export default usersRouter;
