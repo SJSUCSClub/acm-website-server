2e742bcb12a891c8e652f8ed00a7fbe92b646c61
ManasC478 <manas.chougule@gmail.com>
2024-11-12 19:37:11 -0800

    feat: merged client

---
+++ client/src/components/molecules/plan-btn/component.tsx
@@ -0,0 +1,22 @@
+import { Btn } from "../../atoms/btn";
+import RightArrow from "/about/rightarrow.svg";
+
+export type IPlanBtnProps = Omit<
+  React.ComponentProps<typeof Btn>,
+  "children" | "href"
+>;
+
+export const PlanBtn: React.FC<IPlanBtnProps> = ({
+  className,
+  variant,
+  ...props
+}) => {
+  return (
+    <Btn className={className} variant={variant} {...props}>
+      <a href="" className="flex gap-2" onClick={(e) => e.preventDefault()}>
+        <span className=""> Choose this plan{variant === "ghost" && " >"}</span>
+        <img src={RightArrow} alt="right arrow" />
+      </a>
+    </Btn>
+  );
+};
