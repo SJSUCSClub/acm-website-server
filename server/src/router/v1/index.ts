5c908aff550d764cdff3aec2c578398ea40a223f
joshuakojko <69828007+joshuakojko@users.noreply.github.com>
2024-11-12 18:33:31 -0800

    Lint fix

--- server/src/router/v1/index.ts
+++ server/src/router/v1/index.ts
@@ -609,7 +609,7 @@ v1App.openapi(
 				and(
 					eq(subscribedCompanies.userId, user.id),
 					eq(subscribedCompanies.companyId, companyId),
-				)
+				),
 			).returning();
 		if (result.length === 0) {
 			return c.json({ error: 'Subscription not found' }, HttpStatusCodes.NOT_FOUND);
