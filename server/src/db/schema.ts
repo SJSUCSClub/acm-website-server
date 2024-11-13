527a53a5b7d8c4eb23d50b40da811e1b4342b802
ManasC478 <85337436+ManasC478@users.noreply.github.com>
2024-11-12 18:31:52 -0800

    feat: added mock data (#53)
    * feat: added mock data
    
    * fix: move insert data, update types
    
    * fix
    
    * separate db files
    
    * Projects, Events, Companies endpoints
    
    * typecheck fix
    
    * Update major api
    
    * edit field num
    
    * Update csFieldsEnum
    
    * Update csFieldEnum
    
    * Equipment endpoints
    
    * feat: add alumni check functionality (#48)
    
    * feat: add alumni check functionality
    
    * feat: implement search functionality
    
    * fix: sql syntax (#50)
    
    * fix: sql syntax
    
    * fix: eslint
    
    * fix: drizzle-zod interests array of enums (#51)
    
    * update package versions (#52)
    
    * update package versions
    
    * feat: csrf and cors for dev
    
    * feat: add alumni check functionality
    
    ---------
    
    Co-authored-by: ManasC478 <85337436+ManasC478@users.noreply.github.com>
    
    * feat: added mock data
    
    ---------
    
    Co-authored-by: knhn1004 <49494541+knhn1004@users.noreply.github.com>
    Co-authored-by: joshuakojko <69828007+joshuakojko@users.noreply.github.com>

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -2,12 +2,12 @@ import { text, timestamp, date, integer, bigint, pgEnum, pgTable, serial, time, 
 
 // Enums
 export const eventsEnum = pgEnum('events_enum', ['workshop', 'seminar', 'hackathon', 'conference', 'meetup', 'test', 'other']);
-export const csFieldsEnum = pgEnum('cs_fields_enum', ['web development', 'machine learning', 'cloud computing', 'artificial intelligence']);
+export const csFieldsEnum = pgEnum('cs_fields_enum', ['web development', 'machine learning', 'cloud computing', 'artificial intelligence', 'networking', 'cybersecurity', 'mobile development', 'game development', 'data science']);
 export const targetAudienceEnum = pgEnum('target_audience_enum', ['students']);
 export const equipmentConditionEnum = pgEnum('equipment_condition_enum', ['ready', 'broken', 'in maintenance']);
 export const membershipTermEnum = pgEnum('membership_term_enum', ['semester', 'annual']);
 export const membershipRequestStatusEnum = pgEnum('membership_request_status_enum', ['pending', 'approved', 'declined']);
-export const industryEnum = pgEnum('industry_enum', ['investment banking', 'aerospace', 'healthcare']);
+export const industryEnum = pgEnum('industry_enum', ['banking and finance', 'aerospace', 'healthcare', 'automotive', 'energy', 'technology']);
 export const officerPositionEnum = pgEnum('officer_position_enum', ['president', 'vice president', 'dev team officer', 'treasurer', 'social media manager']);
 
 // Tables
@@ -49,6 +49,7 @@ export const equipmentRentalType = pgTable('equipment_rental_type', {
   createdAt: timestamp('created_at').notNull().defaultNow(),
   name: text('name').notNull(),
   price: numeric('price', { precision: 10, scale: 2 }).notNull(),
+  image: text('image'),
   description: text('description'),
 });
 
