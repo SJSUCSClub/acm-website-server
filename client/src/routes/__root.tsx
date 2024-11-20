f03513aa89c17fddfd287a529dcd58f6418eff6a
Oliver (Chia-Hong) Chou <49494541+knhn1004@users.noreply.github.com>
2024-11-19 18:48:26 -0800

    Feature/merge client (#58)
    * feat: merged client
    
    * setup nginx
    
    * remove file
    
    * fix: update Dockerfile for development environment
    
    * fix: rewrite URL routing for API endpoint
    
    ---------
    
    Co-authored-by: ManasC478 <manas.chougule@gmail.com>

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+import "../globals.css";
+import Footer from "../components/atoms/footer";
+import Navbar from "../components/organisms/navbar";
+import Faq from "../components/organisms/faq";
+import "acm-cs-sjsu-hero-component/dist/styles.css";
+
+const queryClient = new QueryClient();
+
+export const Route = createRootRoute({
+  component: () => (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Navbar />
+        <Outlet />
+        <div className="flex">
+          <Faq />
+        </div>
+        <Footer />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && <TanStackRouterDevtools />}
+    </QueryClientProvider>
+  ),
+});
