5c1b5a629fb12d854c31a36fdc3bf17ee106be9a
ManasC478 <manas.chougule@gmail.com>
2024-11-26 01:24:20 -0800

    delete duplication function

--- db-scripts/01_createdb.sql
+++ db-scripts/01_createdb.sql
@@ -314,24 +314,6 @@ BEGIN
 END;
 $$;
 
-create or replace function getEnumValues(enumName text)
-returns text[]
-language plpgsql
-as
-$$
-declare
-   exist boolean;
-	values text[];
-begin
-   select exists (select 1 from pg_type where typname = enumName) into exist;
-   if exist then
-      select array(select enumlabel from pg_enum where enumtypid=enumName::regtype) into values;
-      return values;
-   end if;
-   return array[]::text[];
-end;
-$$;
-
 CREATE OR REPLACE FUNCTION getYear(userId text)
 RETURNS year_enum 
 LANGUAGE plpgsql
