e8320405a90986685690c678ee40727fe0e7aabc
Naya Singhania <naya.singhania@gmail.com>
2025-02-11 18:47:05 -0800

    event card layout

--- client/src/components/molecules/event-card/component.tsx
+++ client/src/components/molecules/event-card/component.tsx
@@ -15,7 +15,9 @@ export type EventCardProps = Omit<
   location: string;
   date: string;
   deadline: string;
+  presenter: string;
   description: string;
+  eventType: string;
   keywords: string[];
 };
 
@@ -24,22 +26,32 @@ export const EventCard: React.FC<EventCardProps> = ({
   location,
   date,
   deadline,
+  presenter,
   description,
+  eventType,
   keywords,
   className,
 }) => {
   return (
-    <Card className={cn(className, "pt-4")}>
-      <CardTitle className="pl-4">{title}</CardTitle>
-      <CardHeader>{date}</CardHeader>
-      <CardHeader>{location}</CardHeader>
-      <CardHeader className="text-destructive">{deadline}</CardHeader>
-      <CardContent>
-        <p>{description}</p>
-      </CardContent>
-      <CardFooter>
+    <Card className={cn(className, "pt-4 shadow-xl")}>
+      <CardTitle className="pl-6">
+        <p className="text-sm text-muted-foreground">
+          {eventType.toUpperCase()}
+        </p>
+        <p className="text-lg">{title}</p>
+      </CardTitle>
+      <CardHeader>
+        <p>Date: {date}</p>
+        <p className="text-destructive font-bold">Deadline: {deadline}</p>
+        <a href="#">Location: {location}</a>
+        <p>Presenter(s): {presenter}</p>
+      </CardHeader>
+      <CardContent>{description}</CardContent>
+      <CardFooter className="grid grid-cols-3 lg:grid-cols-2 xl:grid-cols-3">
         {keywords.map((keyword, index) => (
-            <p className="pr-4" key={index}>{keyword}</p>
+          <Btn variant="secondary" className="bg-[#318BCF]" key={index}>
+            <p className="text-xs">{keyword}</p>
+          </Btn>
         ))}
       </CardFooter>
     </Card>
