2d08eba083dbfbb85e53db9027d66f838e0441fe
Naya Singhania <naya.singhania@gmail.com>
2025-02-18 18:30:52 -0800

    update api call

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -34,7 +34,7 @@ const CalendarPage = () => {
     return `${formattedHour}:${minutes} ${ampm}`;
   }
   useEffect(() => {
-    fetch("http://localhost/api/v1/events", {
+    fetch("/api/v1/events", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
