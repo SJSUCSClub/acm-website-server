33ff269709520d5e0b386349f2c01c2aa51ad4d0
Naya Singhania <naya.singhania@gmail.com>
2025-02-18 19:01:04 -0800

    basic date filter ui

--- client/src/components/molecules/btn-date-filter/component.tsx
+++ client/src/components/molecules/btn-date-filter/component.tsx
@@ -1,62 +1,21 @@
-import { ChevronsUpDown } from "lucide-react";
-import * as React from "react";
-import { Button } from "../../../components/ui/button";
-import {
-  Command,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "../../../components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../../../components/ui/popover";
+import { Tabs, TabsList, TabsTrigger } from "../../../components/ui/tabs";
 
 export type EventCardProps = {
-  fcn: React.Dispatch<React.SetStateAction<"upcoming" | "today" | "past" | "all">>;
+  tab1: string;
+  tab2: string;
 };
 
-export const BtnDateFilter: React.FC<EventCardProps> = ({
-  fcn,
-}) => {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("All");
-  const options = ["All", "Upcoming", "Today", "Past"];
+export const BtnDateFilter: React.FC<EventCardProps> = ({ tab1, tab2 }) => {
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value}
-          <ChevronsUpDown className="opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandList>
-            <CommandGroup>
-              {options.map((option) => (
-                <CommandItem
-                  key={option}
-                  value={option}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue);
-                    setOpen(false);
-                    fcn(currentValue.toLowerCase() as "upcoming" | "today" | "past" | "all");
-                  }}
-                >
-                  {option}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <Tabs className="w-[35%] md:w-[20%] text-center">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account" className="text-xs">
+          {tab1}
+        </TabsTrigger>
+        <TabsTrigger value="password" className="text-xs">
+          {tab2}
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
   );
 };
