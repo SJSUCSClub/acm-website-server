1bef5b93f0086b1f28e392c1c9afd8b10c0eee3b
Naya Singhania <naya.singhania@gmail.com>
2025-02-18 19:01:04 -0800

    basic date filter ui

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import EventCard from "../../components/molecules/event-card";
+import BtnDateFilter from "../../components/molecules/btn-date-filter";
 
 interface Event {
   description: string;
@@ -59,7 +60,9 @@ const CalendarPage = () => {
             These events are accessible to all those who are interested,
             irrespective of their major or prior experience.
           </p>
+          <BtnDateFilter tab1="Upcoming" tab2="Past" />
         </div>
+
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
           {events.map((event) => (
             <EventCard
