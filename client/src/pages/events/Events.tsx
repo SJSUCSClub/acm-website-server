d50c363c3f966a4c8c85ca8b5abef10470a8dbec
Naya Singhania <naya.singhania@gmail.com>
2025-02-18 18:20:13 -0800

    use data from server

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -35,7 +35,7 @@ const CalendarPage = () => {
     return `${formattedHour}:${minutes} ${ampm}`;
   }
   useEffect(() => {
-    fetch("/api/v1/events", {
+    fetch("http://localhost/api/v1/events", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
