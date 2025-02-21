8569d82de97e9590b2e55b7d97571219a5310e99
Aarya Chamkeri <aaryachamkeri06@gmail.com>
2025-02-21 15:09:17 -0800

    Separated upload and delete commands and iam

---
+++ server/src/lib/aws/s3.ts
@@ -0,0 +1,36 @@
+import { DeleteObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getS3Client } from "./iam";
+
+const BUCKET_NAME = 'acmwebsite-dev-588738592350-us-west-2';
+
+const uploadFile = async (file: File): Promise<Boolean> => {
+    const s3client: S3Client | Boolean = await getS3Client();
+    if(s3client == false) {
+        return false;
+    } else {
+        try {
+            const uploadObjectCommand = new PutObjectCommand({Bucket: BUCKET_NAME, Key: file.name, Body: (await file.arrayBuffer())});
+            await (<S3Client>s3client).send(uploadObjectCommand);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+}
+
+const deleteFile = async (key: string): Promise<Boolean> => {
+    const s3client: S3Client | Boolean = await getS3Client();
+    if(s3client == false) {
+        return false;
+    } else {
+        try {
+            const deleteObjectCommand = new DeleteObjectCommand({Bucket: BUCKET_NAME, Key: key});
+            await (<S3Client>s3client).send(deleteObjectCommand);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+}
+
+export { uploadFile, deleteFile };
