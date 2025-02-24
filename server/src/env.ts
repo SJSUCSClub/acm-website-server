cd5bb2ef84e32ea0e0058f2b72544bef3c58aeab
Aarya Chamkeri <aaryachamkeri06@gmail.com>
2025-02-24 14:37:45 -0800

    Fixing lint check

--- server/src/env.ts
+++ server/src/env.ts
@@ -14,7 +14,7 @@ const EnvSchema = z.object({
 	GOOGLE_CLIENT_SECRET: z.string().default(''),
 	GOOGLE_REDIRECT_URI: z.string().default(''),
 	ACCESS_KEY_ID: z.string().default(''),
-	SECRET_ACCESS_KEY: z.string().default('')
+	SECRET_ACCESS_KEY: z.string().default(''),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
