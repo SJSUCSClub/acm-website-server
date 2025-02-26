6a471527fb8e7fce00dfb2e08c6a10900f14dbe4
ManasC478 <85337436+ManasC478@users.noreply.github.com>
2025-02-25 18:21:21 -0800

    Feature/landing page data (#87)
    * feat: landing page data
    
    * feat: created clubLinks routes
    
    * feat: added spotlight api
    
    * feat: added questions api
    
    * fix: typecheck

--- server/src/util/zod.ts
+++ server/src/util/zod.ts
@@ -1,5 +1,5 @@
 import { createSelectSchema } from 'drizzle-zod';
-import { educationLevelEnum, bookmarkedEvents, projects, users, events, subscribedCompanies, majors, companies, equipmentRentalType, equipmentItem, equipmentRentals, files, sponsors, officers, csFieldsEnum } from '@/db/schema';
+import { educationLevelEnum, bookmarkedEvents, projects, users, events, subscribedCompanies, majors, companies, equipmentRentalType, equipmentItem, equipmentRentals, files, sponsors, officers, csFieldsEnum, clubLinks, landingQuestions, landingSpotlights } from '@/db/schema';
 
 import { z } from 'zod';
 
@@ -55,3 +55,6 @@ export const equipmentTypeIdSchema = z.object({
     }),
 });
 export const officerSchema = createSelectSchema(officers);
+export const clubLinkSchema = createSelectSchema(clubLinks);
+export const landingSpotlightSchema = createSelectSchema(landingSpotlights);
+export const landingQuestionSchema = createSelectSchema(landingQuestions);
