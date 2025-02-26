de8e9cad021dd317b5c4d0548d8a020c57016d87
Naya Singhania <naya.singhania@gmail.com>
2025-02-25 18:52:02 -0800

    basic date filter dropdown

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -1,8 +1,10 @@
+"use client"
+
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../utils/cn";
 
 const Dialog = DialogPrimitive.Root
 
@@ -110,8 +112,8 @@ export {
   Dialog,
   DialogPortal,
   DialogOverlay,
-  DialogTrigger,
   DialogClose,
+  DialogTrigger,
   DialogContent,
   DialogHeader,
   DialogFooter,
