6e399a3f16a7e5ebad3f9c99992036488386cbda
ManasC478 <manas.chougule@gmail.com>
2025-02-26 22:22:32 -0800

    feat: integrated react-query and update components

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import { paths } from "@/types/schema.v1";
+import createFetchClient, { Middleware } from "openapi-fetch";
+import { redirect } from '@tanstack/react-router'
+import createClient from "openapi-react-query";
+
+const api = createFetchClient<paths>();
+
+const middleware: Middleware = {
+  async onResponse({  response }) {
+    console.log("API middleware response: ", response.status);
+    if (!response.ok) {
+      if (response.status === 401) {
+        throw redirect({
+          to: "/login"
+        });
+      }
+      else if (response.status === 403) {
+        throw redirect({
+          to: "/login"
+        });
+      }
+    }
+    return response;
+  },
+  async onError({error}) {
+    console.log("API middleware error: ", error);
+    return;
+  }
+}
+
+api.use(middleware);
+const $api = createClient(api);
+
+export const useQuery = $api.useQuery;
+export const useMutation = $api.useMutation;
+
