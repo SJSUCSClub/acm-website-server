a7e477e443656d2e6b3c52ca53aff482b9ba2be8
ManasC478 <manas.chougule@gmail.com>
2025-03-29 14:13:54 -0700

    fix: modified spinner

--- client/src/components/atoms/spinner/component.tsx
+++ client/src/components/atoms/spinner/component.tsx
@@ -1,9 +1,16 @@
+import { cn } from '@/utils/cn';
 import type React from 'react';
 
-export const Spinner: React.FC = () => {
+export interface ISpinnerProps extends React.HTMLAttributes<HTMLDivElement> { }
+
+export const Spinner: React.FC<ISpinnerProps> = ({ className, ...props }) => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-    </div>
+    <div
+      {...props}
+      className={cn(
+        'animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900',
+        className
+      )}
+    ></div>
   );
 };
