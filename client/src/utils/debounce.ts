23c3c88ddd7a723e3aabdfb52d97a4112543a06a
ManasC478 <manas.chougule@gmail.com>
2025-04-10 17:52:52 -0700

    fix: format

--- client/src/utils/debounce.ts
+++ client/src/utils/debounce.ts
@@ -3,7 +3,7 @@ export default function debounce<T extends (...args: any[]) => void>(
   delay: number
 ): (...args: Parameters<T>) => void {
   let timer: ReturnType<typeof setTimeout>;
-  
+
   return (...args: Parameters<T>) => {
     clearTimeout(timer);
     timer = setTimeout(() => func(...args), delay);
