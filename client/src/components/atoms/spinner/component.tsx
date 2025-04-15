b89d8b44c01dde896cc3406ecdf93a2c94598de0
ManasC478 <manas.chougule@gmail.com>
2025-04-14 17:11:16 -0700

    fix: update spinner type

--- client/src/components/atoms/spinner/component.tsx
+++ client/src/components/atoms/spinner/component.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/utils/cn';
 import type React from 'react';
 
-export const Spinner: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+export type SpinnerProps = React.HTMLAttributes<HTMLDivElement>;
+
+export const Spinner: React.FC<SpinnerProps> = ({
   className,
   ...props
 }) => {
