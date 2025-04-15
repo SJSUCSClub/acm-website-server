bbf82fe9e27034ac5cf847f76c8138803f84a64c
ManasC478 <manas.chougule@gmail.com>
2025-04-14 17:31:01 -0700

    feat: update project

--- client/src/components/organisms/project-form/component.tsx
+++ client/src/components/organisms/project-form/component.tsx
@@ -1,5 +1,6 @@
 import Btn from '@/components/atoms/btn';
 import FieldErrorMessage from '@/components/atoms/field-error-message';
+import FetchError from '@/components/molecules/fetch-error';
 import Loading from '@/components/molecules/loading';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -50,6 +51,7 @@ const ProjectForm: React.FC<IProjectFormProps> = ({ projectId = null }) => {
     }
   );
   const { mutate: createProject } = useMutation('post', '/v1/projects');
+  const { mutate: updateProject } = useMutation('put', '/v1/projects/{projectID}');
 
   const form = useForm({
     defaultValues: {
@@ -62,127 +64,158 @@ const ProjectForm: React.FC<IProjectFormProps> = ({ projectId = null }) => {
       onChange: formSchema
     },
     onSubmit: ({ value }) => {
-      createProject(
-        {
-          body: {
-            ...value,
-            githubLink: value.githubLink || null
-          }
-        },
-        {
-          onSuccess(data) {
-            navigate({
-              to: '/admin/projects/$projectId',
-              params: { projectId: data.project.id.toString() }
-            });
+      if (projectId) {
+        updateProject(
+          {
+            params: {
+              path: {
+                projectID: projectId
+              }
+            },
+            body: {
+              ...value,
+              githubLink: value.githubLink || null
+            }
           },
-          onError() {
-            toast.error('Failed to create project');
+          {
+            onSuccess() {
+              navigate({
+                to: '/admin/projects/$projectId',
+                params: { projectId: projectId }
+              });
+            },
+            onError() {
+              toast.error('Failed to update project');
+            }
           }
-        }
-      );
+        );
+      } else {
+        createProject(
+          {
+            body: {
+              ...value,
+              githubLink: value.githubLink || null
+            }
+          },
+          {
+            onSuccess(data) {
+              navigate({
+                to: '/admin/projects/$projectId',
+                params: { projectId: data.project.id.toString() }
+              });
+            },
+            onError() {
+              toast.error('Failed to create project');
+            }
+          }
+        );
+      }
     }
   });
 
   return (
     <Loading isLoading={isLoading}>
-      <div>
-        <h1 className="text-4xl font-bold mb-5">{projectId ? 'Edit Project' : 'Create Project'}</h1>
-        <form
-          className="space-y-5"
-          onSubmit={(e) => {
-            e.preventDefault();
-            form.handleSubmit();
-          }}
-        >
-          <form.Field
-            name="name"
-            children={(field) => (
-              <div>
-                <Label htmlFor={field.name} className="text-lg">
-                  Name
-                </Label>
-                <Input
-                  placeholder="Enter project name"
-                  name={field.name}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  type="text"
-                  className="w-full"
-                />
-                <FieldErrorMessage field={field} />
-              </div>
-            )}
-          />
+      <FetchError isError={!!error}>
+        <div>
+          <h1 className="text-4xl font-bold mb-5">
+            {projectId ? 'Edit Project' : 'Create Project'}
+          </h1>
+          <form
+            className="space-y-5"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+          >
+            <form.Field
+              name="name"
+              children={(field) => (
+                <div>
+                  <Label htmlFor={field.name} className="text-lg">
+                    Name
+                  </Label>
+                  <Input
+                    placeholder="Enter project name"
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    type="text"
+                    className="w-full"
+                  />
+                  <FieldErrorMessage field={field} />
+                </div>
+              )}
+            />
 
-          <form.Field
-            name="description"
-            children={(field) => (
-              <div>
-                <Label htmlFor={field.name} className="text-lg">
-                  Description
-                </Label>
-                <Textarea
-                  placeholder="Describe your project"
-                  className="min-h-[120px] w-full"
-                  name={field.name}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                <FieldErrorMessage field={field} />
-              </div>
-            )}
-          />
+            <form.Field
+              name="description"
+              children={(field) => (
+                <div>
+                  <Label htmlFor={field.name} className="text-lg">
+                    Description
+                  </Label>
+                  <Textarea
+                    placeholder="Describe your project"
+                    className="min-h-[120px] w-full"
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  <FieldErrorMessage field={field} />
+                </div>
+              )}
+            />
 
-          <form.Field
-            name="githubLink"
-            children={(field) => (
-              <div>
-                <Label htmlFor={field.name} className="text-lg">
-                  GitHub Link
-                </Label>
-                <Input
-                  placeholder="https://github.com/username/repo"
-                  name={field.name}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  type="text"
-                  className="w-full"
-                />
-                <FieldErrorMessage field={field} />
-              </div>
-            )}
-          />
+            <form.Field
+              name="githubLink"
+              children={(field) => (
+                <div>
+                  <Label htmlFor={field.name} className="text-lg">
+                    GitHub Link
+                  </Label>
+                  <Input
+                    placeholder="https://github.com/username/repo"
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    type="text"
+                    className="w-full"
+                  />
+                  <FieldErrorMessage field={field} />
+                </div>
+              )}
+            />
 
-          <form.Field
-            name="status"
-            children={(field) => (
-              <div>
-                <Label htmlFor={field.name} className="text-lg">
-                  Status
-                </Label>
-                <Select
-                  onValueChange={(val) => field.handleChange(val as Project['status'])}
-                  defaultValue={field.state.value}
-                  name={field.name}
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Select project status" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="Not Started">Not Started</SelectItem>
-                    <SelectItem value="In Progress">In Progress</SelectItem>
-                    <SelectItem value="Completed">Completed</SelectItem>
-                    <SelectItem value="Looking for Members">Looking for Members</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FieldErrorMessage field={field} />
-              </div>
-            )}
-          />
-          <Btn type="submit">{projectId ? 'Update' : 'Create'}</Btn>
-        </form>
-      </div>
+            <form.Field
+              name="status"
+              children={(field) => (
+                <div>
+                  <Label htmlFor={field.name} className="text-lg">
+                    Status
+                  </Label>
+                  <Select
+                    onValueChange={(val) => field.handleChange(val as Project['status'])}
+                    defaultValue={field.state.value}
+                    name={field.name}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select project status" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Not Started">Not Started</SelectItem>
+                      <SelectItem value="In Progress">In Progress</SelectItem>
+                      <SelectItem value="Completed">Completed</SelectItem>
+                      <SelectItem value="Looking for Members">Looking for Members</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FieldErrorMessage field={field} />
+                </div>
+              )}
+            />
+            <Btn type="submit">{projectId ? 'Update' : 'Create'}</Btn>
+          </form>
+        </div>
+      </FetchError>
     </Loading>
   );
 };
