0a688b39089721cf1ab9aac2b5c95be9a6bb6734
ManasC478 <manas.chougule@gmail.com>
2025-04-14 17:13:04 -0700

    feat: project create/edit routes

---
+++ client/src/routes/admin/_layout/projects/$projectId/index.tsx
@@ -0,0 +1,11 @@
+import ProjectDetails from '@/components/organisms/project-details';
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/admin/_layout/projects/$projectId/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const { projectId } = Route.useParams();
+  return <ProjectDetails projectId={projectId} admin />;
+}
---
+++ client/src/routes/admin/_layout/projects/create.tsx
@@ -0,0 +1,10 @@
+import { ProjectForm } from '@/components/organisms/project-form'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/admin/_layout/projects/create')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <ProjectForm />
+}
