79a22c66c69b9e3801f1860b36cd465e67d9ed5b
Naya Singhania <naya.singhania@gmail.com>
2025-04-15 19:30:30 -0700

    fix formatting on client

--- client/src/pages/admin/Officers.tsx
+++ client/src/pages/admin/Officers.tsx
@@ -8,24 +8,24 @@ type Officer =
 const Officers = () => {
   const { data: officers } = useQuery('get', '/v1/officers');
   return (
-      <div className="about text-text">
-        <div className="devteam">
-          <h1 className="text-4xl text-center font-bold my-8">Officers</h1>
-          <div className="flex flex-col items-center justify-center gap-16 sm:gap-8 mx-[-10%]">
-            <div className="sm:flex sm:flex-wrap gap-x-10 items-center justify-center">
-              {officers?.officers.map((officer: Officer) => (
-                <OfficerCard
-                  key={officer.id}
-                  name={officer.userId}
-                  position={officer.position}
-                  photo={officer.photo || ''}
-                  linkedin={officer.linkedin || ''}
-                />
-              ))}
-            </div>
+    <div className="about text-text">
+      <div className="devteam">
+        <h1 className="text-4xl text-center font-bold my-8">Officers</h1>
+        <div className="flex flex-col items-center justify-center gap-16 sm:gap-8 mx-[-10%]">
+          <div className="sm:flex sm:flex-wrap gap-x-10 items-center justify-center">
+            {officers?.officers.map((officer: Officer) => (
+              <OfficerCard
+                key={officer.id}
+                name={officer.userId}
+                position={officer.position}
+                photo={officer.photo || ''}
+                linkedin={officer.linkedin || ''}
+              />
+            ))}
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
