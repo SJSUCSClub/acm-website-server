27765eeac0d7702c9ee80aebdf8826bce236d3fa
ManasC478 <manas.chougule@gmail.com>
2025-04-16 14:16:56 -0700

    fix: lint

--- server/src/router/v1/projects.ts
+++ server/src/router/v1/projects.ts
@@ -1,23 +1,23 @@
-import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
-import { z } from "zod";
-import * as HttpStatusCodes from "stoker/http-status-codes";
+import { OpenAPIHono, createRoute } from '@hono/zod-openapi';
+import { z } from 'zod';
+import * as HttpStatusCodes from 'stoker/http-status-codes';
 
-import type { Context } from "@/lib/context";
-import { db } from "@/db/db";
+import type { Context } from '@/lib/context';
+import { db } from '@/db/db';
 import {
   users,
   projects,
   files,
   interestedInProjects,
   projectsFiles,
-} from "@/db/schema";
-import { and, eq, getTableColumns } from "drizzle-orm";
-import type { User, Project, File as FileSchema } from "@/db/schema"; // naming conflict with File and schema File type
+} from '@/db/schema';
+import { and, eq, getTableColumns } from 'drizzle-orm';
+import type { User, Project, File as FileSchema } from '@/db/schema'; // naming conflict with File and schema File type
 import {
   authMiddleWare,
   unauthorizedRequest,
   forbiddenRequest,
-} from "@/middlewares/auth-middleware";
+} from '@/middlewares/auth-middleware';
 import {
   projectIDSchema,
   userSchema,
@@ -25,12 +25,12 @@ import {
   projectSchema,
   errorSchema,
   newProjectSchema,
-} from "@/util/zod";
+} from '@/util/zod';
 import {
   deleteFile,
   generateObjectUrl,
   getPresignedUrlPutObj,
-} from "@/lib/aws/s3";
+} from '@/lib/aws/s3';
 
 const projectRouter = new OpenAPIHono<Context>();
 
@@ -41,11 +41,11 @@ const generateFileKey = (
 
 projectRouter.openapi(
   createRoute({
-    method: "post",
-    path: "/{projectID}/files/{filename}",
-    tags: ["projects"],
-    summary: "Upload a file to a project",
-    middleware: [authMiddleWare("admin")],
+    method: 'post',
+    path: '/{projectID}/files/{filename}',
+    tags: ['projects'],
+    summary: 'Upload a file to a project',
+    middleware: [authMiddleWare('admin')],
     request: {
       params: z.object({
         projectID: projectIDSchema.shape.projectID,
@@ -55,40 +55,40 @@ projectRouter.openapi(
     responses: {
       [HttpStatusCodes.OK]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               presigned_url: z.string(),
             }),
           },
         },
-        description: "Successful",
+        description: 'Successful',
       },
       [HttpStatusCodes.BAD_REQUEST]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Bad request",
+        description: 'Bad request',
       },
       [HttpStatusCodes.INTERNAL_SERVER_ERROR]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Internal server error",
+        description: 'Internal server error',
       },
       ...forbiddenRequest,
       ...unauthorizedRequest,
     },
   }),
   async (c) => {
-    const projectId = c.req.param("projectID");
-    const filename = c.req.param("filename");
+    const projectId = c.req.param('projectID');
+    const filename = c.req.param('filename');
     if (!projectId || !filename) {
       return c.json(
-        { error: "error occured uploading file" },
+        { error: 'error occured uploading file' },
         HttpStatusCodes.BAD_REQUEST,
       );
     }
@@ -103,13 +103,13 @@ projectRouter.openapi(
 
       const res = await getPresignedUrlPutObj(key);
       if (!res) {
-        throw new Error("Failed to generate presigned url");
+        throw new Error('Failed to generate presigned url');
       }
       return c.json({ presigned_url: res }, HttpStatusCodes.OK);
     } catch (error) {
       console.log(error);
       return c.json(
-        { error: "error generating presigned url" },
+        { error: 'error generating presigned url' },
         HttpStatusCodes.INTERNAL_SERVER_ERROR,
       );
     }
@@ -118,11 +118,11 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "delete",
-    path: "/{projectID}/files/{fileName}",
-    tags: ["projects"],
-    summary: "Delete a file from a project",
-    middleware: [authMiddleWare("admin")],
+    method: 'delete',
+    path: '/{projectID}/files/{fileName}',
+    tags: ['projects'],
+    summary: 'Delete a file from a project',
+    middleware: [authMiddleWare('admin')],
     request: {
       params: z.object({
         projectID: projectIDSchema.shape.projectID,
@@ -131,35 +131,35 @@ projectRouter.openapi(
     },
     responses: {
       [HttpStatusCodes.NO_CONTENT]: {
-        description: "Successful response",
+        description: 'Successful response',
       },
       [HttpStatusCodes.BAD_REQUEST]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Bad request",
+        description: 'Bad request',
       },
       [HttpStatusCodes.INTERNAL_SERVER_ERROR]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Internal server error",
+        description: 'Internal server error',
       },
       ...unauthorizedRequest,
       ...forbiddenRequest,
     },
   }),
   async (c) => {
-    const fileName = c.req.param("fileName");
-    const projectId = c.req.param("projectID");
+    const fileName = c.req.param('fileName');
+    const projectId = c.req.param('projectID');
 
     if (!fileName || !projectId) {
       return c.json(
-        { error: "Not valid parameters" },
+        { error: 'Not valid parameters' },
         HttpStatusCodes.BAD_REQUEST,
       );
     }
@@ -177,9 +177,9 @@ projectRouter.openapi(
       await db.delete(files).where(eq(files.key, fileKey));
       const res = await deleteFile(fileKey);
       if (!res) {
-        throw new Error("Failed to delete file");
+        throw new Error('Failed to delete file');
       }
-      return c.text("", HttpStatusCodes.NO_CONTENT);
+      return c.text('', HttpStatusCodes.NO_CONTENT);
     } catch (error) {
       return c.json(
         { error: `error occured deleting file: ${error}` },
@@ -191,20 +191,20 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "get",
-    path: "/",
-    tags: ["projects"],
-    summary: "List all projects",
+    method: 'get',
+    path: '/',
+    tags: ['projects'],
+    summary: 'List all projects',
     responses: {
       [HttpStatusCodes.OK]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               projects: z.array(projectSchema),
             }),
           },
         },
-        description: "Successful response",
+        description: 'Successful response',
       },
     },
   }),
@@ -216,43 +216,43 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "get",
-    path: "/{projectID}",
-    tags: ["projects"],
-    summary: "Get a project by ID",
+    method: 'get',
+    path: '/{projectID}',
+    tags: ['projects'],
+    summary: 'Get a project by ID',
     request: {
       params: projectIDSchema,
     },
     responses: {
       [HttpStatusCodes.OK]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               project: projectSchema,
             }),
           },
         },
-        description: "Successful response",
+        description: 'Successful response',
       },
       [HttpStatusCodes.NOT_FOUND]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Project not found",
+        description: 'Project not found',
       },
     },
   }),
   async (c) => {
-    const { projectID } = c.req.valid("param");
+    const { projectID } = c.req.valid('param');
     const project = await db
       .select()
       .from(projects)
       .where(eq(projects.id, parseInt(projectID)));
 
     if (!project.length) {
-      return c.json({ error: "Project not found" }, HttpStatusCodes.NOT_FOUND);
+      return c.json({ error: 'Project not found' }, HttpStatusCodes.NOT_FOUND);
     }
 
     return c.json({ project: project[0] }, HttpStatusCodes.OK);
@@ -261,29 +261,29 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "get",
-    path: "/{projectID}/interested",
-    tags: ["projects"],
-    summary: "List all interested users for a project",
-    middleware: [authMiddleWare("admin")],
+    method: 'get',
+    path: '/{projectID}/interested',
+    tags: ['projects'],
+    summary: 'List all interested users for a project',
+    middleware: [authMiddleWare('admin')],
     request: {
       params: projectIDSchema,
     },
     responses: {
       [HttpStatusCodes.OK]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               interestedUsers: z.array(userSchema),
             }),
           },
         },
-        description: "Successful response",
+        description: 'Successful response',
       },
     },
   }),
   async (c) => {
-    const { projectID } = c.req.valid("param");
+    const { projectID } = c.req.valid('param');
     const interestedUsers: User[] = await db
       .select(getTableColumns(users))
       .from(interestedInProjects)
@@ -304,28 +304,28 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "get",
-    path: "/{projectID}/files",
-    tags: ["projects"],
-    summary: "List all files for a project",
+    method: 'get',
+    path: '/{projectID}/files',
+    tags: ['projects'],
+    summary: 'List all files for a project',
     request: {
       params: projectIDSchema,
     },
     responses: {
       [HttpStatusCodes.OK]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               projectFiles: z.array(fileSchema),
             }),
           },
         },
-        description: "Successful response",
+        description: 'Successful response',
       },
     },
   }),
   async (c) => {
-    const { projectID } = c.req.valid("param");
+    const { projectID } = c.req.valid('param');
     const projectFiles: FileSchema[] = await db
       .select(getTableColumns(files))
       .from(projectsFiles)
@@ -343,15 +343,15 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "post",
-    path: "/",
-    tags: ["projects"],
-    summary: "Creates a new project",
-    middleware: [authMiddleWare("admin")],
+    method: 'post',
+    path: '/',
+    tags: ['projects'],
+    summary: 'Creates a new project',
+    middleware: [authMiddleWare('admin')],
     request: {
       body: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: newProjectSchema,
           },
         },
@@ -360,32 +360,32 @@ projectRouter.openapi(
     responses: {
       [HttpStatusCodes.CREATED]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: z.object({
               project: projectSchema,
             }),
           },
         },
-        description: "Successful response",
+        description: 'Successful response',
       },
       [HttpStatusCodes.INTERNAL_SERVER_ERROR]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Failed to create project",
+        description: 'Failed to create project',
       },
       ...unauthorizedRequest,
       ...forbiddenRequest,
     },
   }),
   async (c) => {
-    const body = c.req.valid("json");
+    const body = c.req.valid('json');
     const newProject = await db.insert(projects).values(body).returning();
     if (newProject.length === 0) {
       return c.json(
-        { error: "Failed to create project" },
+        { error: 'Failed to create project' },
         HttpStatusCodes.INTERNAL_SERVER_ERROR,
       );
     }
@@ -395,16 +395,16 @@ projectRouter.openapi(
 
 projectRouter.openapi(
   createRoute({
-    method: "put",
-    path: "/{projectID}",
-    tags: ["projects"],
-    summary: "Update project",
-    middleware: [authMiddleWare("admin")],
+    method: 'put',
+    path: '/{projectID}',
+    tags: ['projects'],
+    summary: 'Update project',
+    middleware: [authMiddleWare('admin')],
     request: {
       params: projectIDSchema,
       body: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: newProjectSchema,
           },
         },
@@ -412,43 +412,43 @@ projectRouter.openapi(
     },
     responses: {
       [HttpStatusCodes.NO_CONTENT]: {
-        description: "Successful response",
+        description: 'Successful response',
       },
       [HttpStatusCodes.INTERNAL_SERVER_ERROR]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Failed to create project",
+        description: 'Failed to create project',
       },
       [HttpStatusCodes.NOT_FOUND]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Failed to update project",
+        description: 'Failed to update project',
       },
       [HttpStatusCodes.BAD_REQUEST]: {
         content: {
-          "application/json": {
+          'application/json': {
             schema: errorSchema,
           },
         },
-        description: "Bad request",
+        description: 'Bad request',
       },
       ...unauthorizedRequest,
       ...forbiddenRequest,
     },
   }),
   async (c) => {
-    const body = c.req.valid("json");
-    const projectID = c.req.param("projectID");
+    const body = c.req.valid('json');
+    const projectID = c.req.param('projectID');
 
     if (!projectID) {
       return c.json(
-        { error: "Project ID is required" },
+        { error: 'Project ID is required' },
         HttpStatusCodes.BAD_REQUEST,
       );
     }
@@ -461,11 +461,11 @@ projectRouter.openapi(
         .returning();
       if (newProject.length === 0) {
         return c.json(
-          { error: "Project not found" },
+          { error: 'Project not found' },
           HttpStatusCodes.NOT_FOUND,
         );
       }
-      return c.text("", HttpStatusCodes.NO_CONTENT);
+      return c.text('', HttpStatusCodes.NO_CONTENT);
     } catch (error) {
       return c.json(
         { error: `Failed to update project: ${error}` },
