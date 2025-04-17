7953cce356d70b6fab92e3f5053fc1cb5cc4c4e4
ManasC478 <manas.chougule@gmail.com>
2025-04-17 12:34:57 -0700

    fix: minor changes

--- client/src/components/organisms/company-list/component.tsx
+++ client/src/components/organisms/company-list/component.tsx
@@ -3,6 +3,8 @@ import Loading from '@/components/molecules/loading';
 import { useQuery } from '@/hooks/useFetch';
 import CompanyCard from '@/components/molecules/company-card';
 import React from 'react';
+import { Link } from '@tanstack/react-router';
+import Btn from '@/components/atoms/btn';
 
 export interface ICompanyListProps {
   admin?: boolean;
@@ -13,6 +15,13 @@ const CompanyList: React.FC<ICompanyListProps> = ({ admin = false }) => {
   return (
     <div className="space-y-5">
       <h1 className="text-4xl font-bold">Companies</h1>
+      {admin && (
+        <div className="flex items-center justify-end">
+          <Link to="/admin/companies/create">
+            <Btn>Create Company</Btn>
+          </Link>
+        </div>
+      )}
       <Loading isLoading={isLoading}>
         <FetchError isError={!!error || !companies}>
           <div className="grid grid-cols-3 gap-4">
