015002189daa7ddc7da251b044ffe01da9443a2d
ManasC478 <manas.chougule@gmail.com>
2025-04-20 13:53:35 -0700

    fix: lint/format

--- client/src/routes/companies/index.tsx
+++ client/src/routes/companies/index.tsx
@@ -1,11 +1,15 @@
-import CompanyList from '@/components/organisms/company-list'
-import Page from '@/components/templates/Page'
-import { createFileRoute } from '@tanstack/react-router'
+import CompanyList from '@/components/organisms/company-list';
+import Page from '@/components/templates/Page';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/companies/')({
-  component: RouteComponent,
-})
+  component: RouteComponent
+});
 
 function RouteComponent() {
-  return <Page><CompanyList /></Page>
+  return (
+    <Page>
+      <CompanyList />
+    </Page>
+  );
 }
