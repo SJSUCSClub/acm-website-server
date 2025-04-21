e070345be322a9bea1971ab1cd35fdc533b15d9b
ManasC478 <manas.chougule@gmail.com>
2025-04-21 14:43:26 -0700

    feat: reorder officers button

--- client/src/components/organisms/officer-list/component.tsx
+++ client/src/components/organisms/officer-list/component.tsx
@@ -1,22 +1,36 @@
 import Btn from '@/components/atoms/btn';
-import Spinner from '@/components/atoms/spinner';
 import FetchError from '@/components/molecules/fetch-error';
 import Loading from '@/components/molecules/loading';
 import { OfficerCard } from '@/components/molecules/officer-card';
+import OfficerReorder from '@/components/molecules/officer-reorder';
 import { useQuery } from '@/hooks/useFetch';
 import { Link } from '@tanstack/react-router';
+import { useState } from 'react';
 
 export interface IOfficerListProps {
   admin?: boolean;
 }
 
 const OfficerList: React.FC<IOfficerListProps> = ({ admin = false }) => {
+  const [open, setOpen] = useState(false);
   const { data: officers, isLoading, error, refetch } = useQuery('get', '/v1/officers');
+
+  const handleOfficerReorder = () => {
+    refetch();
+    setOpen(false);
+  };
   return (
     <div className="space-y-5">
       <h1 className="text-4xl font-bold">Officers</h1>
       {admin && (
-        <div className="flex items-center justify-end">
+        <div className="flex items-center justify-end gap-2">
+          <OfficerReorder
+            onOfficerReorder={handleOfficerReorder}
+            open={open}
+            onOpenChange={setOpen}
+          >
+            <Btn variant="outline">Reorder</Btn>
+          </OfficerReorder>
           <Link to="/admin/officers/create">
             <Btn>Create Officer</Btn>
           </Link>
