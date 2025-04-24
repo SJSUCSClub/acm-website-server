16f026343fe4a338d5a0125ea50eef5a67b3e086
ManasC478 <manas.chougule@gmail.com>
2025-04-24 11:41:16 -0700

    feat: create/delete sponsors

--- client/src/components/organisms/sponsor-list/component.tsx
+++ client/src/components/organisms/sponsor-list/component.tsx
@@ -1,21 +1,38 @@
-import Spinner from '@/components/atoms/spinner';
+import FetchError from '@/components/molecules/fetch-error';
+import Loading from '@/components/molecules/loading';
 import SponsorCard from '@/components/molecules/sponsor-card';
+import SponsorForm from '@/components/molecules/sponsor-form';
 import { useQuery } from '@/hooks/useFetch';
 import React from 'react';
 
-const SponsorList = () => {
-  const { data, error, isLoading } = useQuery('get', '/v1/sponsors');
+export interface ISponsorListProps {
+  admin?: boolean;
+}
+
+const SponsorList: React.FC<ISponsorListProps> = ({ admin = false }) => {
+  const { data, error, isLoading, refetch } = useQuery('get', '/v1/sponsors');
+
   return (
     <div>
-      {isLoading ? (
-        <Spinner />
-      ) : error ? (
-        <p>Error loading sponsors</p>
-      ) : (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {data?.sponsors.map((sponsor) => <SponsorCard key={sponsor.name} sponsor={sponsor} />)}
+      {admin && (
+        <div className="flex items-center justify-end gap-2">
+          <SponsorForm onCreateComplete={refetch} />
         </div>
       )}
+      <Loading isLoading={isLoading}>
+        <FetchError isError={!!error}>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 gap-10">
+            {data?.sponsors.map((sponsor) => (
+              <SponsorCard
+                key={sponsor.name}
+                sponsor={sponsor}
+                admin={admin}
+                onSponsorDelete={refetch}
+              />
+            ))}
+          </div>
+        </FetchError>
+      </Loading>
     </div>
   );
 };
