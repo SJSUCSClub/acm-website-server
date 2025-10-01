b16327cfd88495f8c47be6d4781a8062183c4a3c
ManasC478 <manas.chougule@gmail.com>
2025-09-30 21:02:04 -0700

    feat: added dev s3 bucket

---
+++ infrastructure/dev/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.10"
+  backend "s3" {
+    bucket       = "terraform-state-storage-440744215929-us-west-2"
+    encrypt      = true
+    use_lockfile = true
+    key          = "service/acm-website/dev/terraform.tfstate"
+    region       = "us-west-2"
+    profile      = "AcmApplicationTerraformRoleForStagingEnvironment"
+  }
+}
---
+++ infrastructure/dev/media-storage/variables.tf
@@ -0,0 +1,4 @@
+variable "acm_application_server_role_arn" {
+  description = "The ARN of the role that the media storage bucket will be attached to"
+  type = string
+}
