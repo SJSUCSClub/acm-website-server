9fbef5db37b63285186864079734d8e61e360e61
Eliot Hall <chrehall68@gmail.com>
2026-08-19 08:28:42 -0700

    fix staging deployment (#218)

--- renew-cert.sh
+++ renew-cert.sh
@@ -5,7 +5,7 @@ NGINX_CONTAINER_NAME=acm-nginx
 
 # Renew certificates
 echo "Running certificate renewal..."
-docker compose -f docker-compose.certbot.yml run --rm certbot renew
+docker compose -f docker-compose.certbot.yml run --rm --no-deps certbot renew
 docker exec $NGINX_CONTAINER_NAME nginx -s reload
 
 echo "Renewal finished"
